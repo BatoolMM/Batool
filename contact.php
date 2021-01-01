288b53ab238bcf61e7f5af753160bba6a7a73b3c
Batool Almarzouq <batool@liverpool.ac.uk>
2021-01-01 18:57:16 +0300

    minor changes to  php form

--- contact.php
+++ contact.php
@@ -1,20 +1,16 @@
-
-
-  Thanks for your message!
-
 <?php
 
-	$userName 		= $_POST['myName'];
-	$userEmail	 	= $_POST['myEmail'];
-	$userMessage 		= $_POST['myMessage'];
+	$name 		= $_POST['myName'];
+	$email	 	= $_POST['myEmail'];
+	$message 		= $_POST['myMessage'];
 
 	$to 			= "batool@liverpool.ac.uk";
 	$subject 		= "Email from my website";
 	$body 			= "Information Submitted:";
 
-	$body .= "\r\n Name: " . $userName;
-	$body .= "\r\n Email: " . $userEmail;
-	$body .= "\r\n Message: " . $userMessage;
+	$body .= "\r\n Name: " . $name;
+	$body .= "\r\n Email: " . $email;
+	$body .= "\r\n Message: " . $message;
 
 	mail($to, $subject, $body);
 ?>
