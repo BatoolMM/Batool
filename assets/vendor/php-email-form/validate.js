b7c55349624d062bafbdd83247eff2385beff00b
Batool Almarzouq <batool@liverpool.ac.uk>
2021-01-01 19:05:45 +0300

    minor changes in  php form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function($) {
   "use strict";
 
   //Contact
-  $('php-email-form').submit(function() {
-   
+  $('form.php-email-form').submit(function() {
+
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
