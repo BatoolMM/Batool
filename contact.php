aa83961eaa1848e9c84631261226889e8b4c12f8
Batool Almarzouq <batool@liverpool.ac.uk>
2021-01-01 18:52:11 +0300

    change php form

--- contact.php
+++ contact.php
@@ -1,64 +1,20 @@
+
+
+  Thanks for your message!
+
 <?php
 
+	$userName 		= $_POST['myName'];
+	$userEmail	 	= $_POST['myEmail'];
+	$userMessage 		= $_POST['myMessage'];
 
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+	$to 			= "batool@liverpool.ac.uk";
+	$subject 		= "Email from my website";
+	$body 			= "Information Submitted:";
 
-  // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = 'batool@liverpool.ac.uk';
+	$body .= "\r\n Name: " . $userName;
+	$body .= "\r\n Email: " . $userEmail;
+	$body .= "\r\n Message: " . $userMessage;
 
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
-
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
-  //
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
-  //
-  // echo $contact->send();
-
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $POST['message'];
-
-  $email_from = 'My home page';
-  $email_subject = 'New Message from the contact form';
-  $email_body = "Name: $name.\n".
-                "Email: $email.\n".
-                "Subject: $subject.\n".
-                "Message: $subject $message.\n";
-
-  $to = "batool@liverpool.ac.uk";
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $email \r\n";
-
-  mail($to, $email_subject, $email_body, $headers);
-
-  header("location: success.html");
-
+	mail($to, $subject, $body);
 ?>
